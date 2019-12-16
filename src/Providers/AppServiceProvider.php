5bb750b06a25c5e2f5ef64819758c2eaa756a3a6
zhangqing <490702087@qq.com>
2019-12-16 11:04:53 +0800

    update

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -44,17 +44,7 @@ use Illuminate\Support\{
 
 class AppServiceProvider extends ServiceProvider
 {
-    protected static $moduleName    = 'luclin';
-    protected static $moduleSpace   = 'Luclin';
 
-    protected static $loaders = [
-        'querier'   => [
-            'Luclin\\Cabin\\Foundation\\Queriers',
-        ],
-        'seeker'   => [
-            'Luclin\\Cabin\\Foundation\\Seekers',
-        ],
-    ];
 
     /**
      * Boorstrap the service provider.
@@ -63,101 +53,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        dump('####787878');
         $this->registerResolving();
-        //parent::boot();
     }
 
     protected function registerResolving(): void {
-        dump('####6767');
         $this->app->resolving(Request::class, function ($request, $app) {
             $request->confirm();
         });
     }
-
-//    protected function declareMacros(): void {
-//        Eloquent\Collection::macro('sortWithIds',
-//            function(array $ids, string $idField = 'id'): Eloquent\Collection
-//        {
-//            $result = new static();
-//            $keyed  = $this->keyBy($idField);
-//            foreach ($ids as $id) {
-//                $result[] = $keyed[$id] ?? null;
-//            }
-//            return $result;
-//        });
-//
-//        Eloquent\Collection::macro('pluckCustom',
-//            function(string $field, callable $fun, ...$arguments)
-//        {
-//            $result = [];
-//            foreach ($this as $item) {
-//                $plucked = $fun($item->$field, ...$arguments);
-//                if (!$plucked) {
-//                    continue;
-//                }
-//
-//                if (is_array($plucked)) {
-//                    array_push($result, ...$plucked);
-//                } else {
-//                    $result[] = $plucked;
-//                }
-//            }
-//            return $result;
-//        });
-//    }
-
-//    protected function registerQueueJobEvent(): void {
-//        Queue::before(function (JobProcessing $event) {
-//            Cabin::clean();
-//            Support\CacheLoader::cleanAll();
-//            \luc\xheaders(true);
-//        });
-//    }
-//
-//    public function register()
-//    {
-//        parent::register();
-//
-//        // 队列系统修改
-//        $this->app->extend('queue', function ($qm) {
-//            return Bus\QueueManager::inherit($qm);
-//        });
-//
-//        // 实现控制器方法参数控制是否需要登录
-//        $this->app->bind('Luclin\Contracts\Auth', function($app) {
-//            return Auth::authenticate();
-//        });
-//
-//        $this->registerContextContracts();
-//        $this->registerSingleton();
-//    }
-//
-//    protected function registerContextContracts() {
-//        // inner service 相关约定的注入
-//        $this->app->bind('context._auth', function($app) {
-//            try {
-//                return $app->make('Luclin\Contracts\Auth');
-//            } catch (AuthenticationException $exc) {
-//                // do nothing..
-//            }
-//            return null;
-//        });
-//    }
-//
-//    protected function registerSingleton() {
-//
-//        $this->app->singleton('luclin.xheaders', function ($app) {
-//            return new XHeaders();
-//        });
-//    }
-//
-//    protected function initModule(): void {
-//        $module = $this->makeModule(__DIR__.'/../..');
-//
-//        // $module->setPathMapping([
-//        //     'tmp'   => '/tmp',
-//        // ]);
-//    }
-
 }
